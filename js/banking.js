95a9bea97dbb1840214c4c7a9c68ef24c47293d0
alamin0561464977 <amin23435678901315@gmail.com>
2022-02-11 11:52:28 +0300

    first commit

---
+++ js/banking.js
@@ -0,0 +1,44 @@
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    depositInput.value = '';
+});
+
+// handle withdraw event handler
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    // set withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = newWithdrawAmount + previousWithdrawTotal;
+    withdrawTotal.innerText = newWithdrawTotal;
+    withdrawInput.value = '';
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const balanceTotalAmount = parseFloat(balanceTotalText);
+    const alamin = balanceTotalAmount - newWithdrawAmount;
+
+    balanceTotal.innerText = alamin;
+
+
+
+
+});
+
